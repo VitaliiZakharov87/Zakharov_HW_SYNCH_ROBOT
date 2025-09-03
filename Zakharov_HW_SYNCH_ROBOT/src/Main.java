36e6fbcf2c2ae1e4b8d659965bf5dbdd5da5fbfd
VitaliiZakharov87 <vitalzs2016@gmail.com>
2025-09-04 00:00:12 +0300

    Update Main.java

--- Zakharov_HW_SYNCH_ROBOT/src/Main.java
+++ Zakharov_HW_SYNCH_ROBOT/src/Main.java
@@ -3,19 +3,38 @@ import java.util.*;
 public class Main {
 
     public static final Map<Integer, Integer> sizeToFreq = new HashMap<>();
+    public static Thread logThread;
 
     public static void main(String[] args) throws InterruptedException {
+
+        logThread = new Thread(() -> {
+            while (!logThread.isInterrupted()) {
+                synchronized (sizeToFreq) {
+                    try {
+                        sizeToFreq.wait();
+                        printMax();
+                        ;
+                    } catch (InterruptedException e) {
+                        System.out.println(e.getMessage());
+                        break;
+                    }
+                }
+            }
+        });
+        logThread.start();
+
         List<Thread> threadList = new ArrayList<>();
         int countThreads = 1000;
         for (int i = 0; i < countThreads; i++) {
             Thread thread = new Thread(() -> {
                 String route = generateRoute("RLRFR", 100);
                 Integer RFreq = (int) route.chars().filter(ch -> ch == 'R').count();
+                System.out.println("Количесто поворотов направо: " + RFreq);
                 synchronized (sizeToFreq) {
                     if (sizeToFreq.containsKey(RFreq)) sizeToFreq.put(RFreq, sizeToFreq.get(RFreq) + 1);
                     else sizeToFreq.put(RFreq, 1);
+                    sizeToFreq.notify();
                 }
-                System.out.println("Количесто поворотов направо: " + RFreq);
             });
             threadList.add(thread);
             thread.start();
@@ -24,17 +43,10 @@ public class Main {
             thread.join();
         }
 
-        Integer maxCount = 0;
-        Integer freq = 0;
-        for (Map.Entry<Integer, Integer> entry : sizeToFreq.entrySet()) {
-            if (entry.getValue() > maxCount) {
-                maxCount = entry.getValue();
-                freq = entry.getKey();
-            }
-        }
-        System.out.println("Самое частое количество повторений " + freq + " (встретилось " + maxCount + " раз)");
-        System.out.println("Другие размеры:");
+        logThread.interrupt();
 
+        printMax();
+        System.out.println("Другие размеры:");
         for (Map.Entry<Integer, Integer> entry : sizeToFreq.entrySet()) {
             System.out.println("- " + entry.getKey() + " (" + entry.getValue() + " раз)");
         }
@@ -47,5 +59,17 @@ public class Main {
             route.append(letters.charAt(random.nextInt(letters.length())));
         }
         return route.toString();
+    }
+
+    public static void printMax() {
+        Integer maxCount = 0;
+        Integer freq = 0;
+        for (Map.Entry<Integer, Integer> entry : sizeToFreq.entrySet()) {
+            if (entry.getValue() > maxCount) {
+                maxCount = entry.getValue();
+                freq = entry.getKey();
+            }
+        }
+        System.out.println("Самое частое количество повторений " + freq + " (встретилось " + maxCount + " раз)");
     }
 }
